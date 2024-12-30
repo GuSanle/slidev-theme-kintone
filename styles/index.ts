012a59fd18310be4d3d1c8fd2dc86a32abd159dc
slgu <slgu@cybozu.cn>
2024-12-30 16:47:21 +0800

    代码调整

--- styles/index.ts
+++ styles/index.ts
@@ -2,3 +2,15 @@
 import "@slidev/client/styles/layouts-base.css";
 import "./layout.css";
 import "./kintone.css";
+// @ts-ignore
+import headerImg from "../assets/header.png";
+// @ts-ignore
+import footerImg from "../assets/footer.png";
+// @ts-ignore
+import logoImg from "../assets/kintone.png";
+
+export const images = {
+  header: `url(${headerImg})`,
+  footer: `url(${footerImg})`,
+  logo: `url(${logoImg})`,
+};
